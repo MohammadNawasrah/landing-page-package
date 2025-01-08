cc3e5aaf47d74920403278b62ce45f2e2886607b
MohammadNawasrah <nawasrahmohammad2000@gmail.com>
2025-01-08 14:47:25 +0300

    fix bug in migration

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit8192c2057b52f0363281a6ef199d63c9
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit8192c2057b52f0363281a6ef199d63c9', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit8192c2057b52f0363281a6ef199d63c9', 'loadClassLoader'));
