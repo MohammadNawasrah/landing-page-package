41072eddd09477d7f44f33f8e1943c5ce82d1e0e
MohammadNawasrah <nawasrahmohammad2000@gmail.com>
2025-01-08 14:39:43 +0300

    add test provider and migration

---
+++ src/LandingPageServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Mnawasrah\LandingPagePackage;
+
+use Illuminate\Support\ServiceProvider;
+
+class LandingPageServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Publish migrations
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+    }
+}
