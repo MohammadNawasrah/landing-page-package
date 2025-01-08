83424ac40a2c262ef250ff1689d90c8ccc4780a0
MohammadNawasrah <nawasrahmohammad2000@gmail.com>
2025-01-08 15:20:51 +0300

    add layout to content page

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -1,1 +1,6 @@
-<h1>{{ $message }}</h1>
+{{-- resources/views/landing-page.blade.php --}}
+@extends(config('landing-page.layout'))  <!-- Dynamically use the layout from config -->
+
+@section('content')
+    <h1>{{ $message }}</h1>
+@endsection
